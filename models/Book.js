d445c9a2f86353c899fe078ad477ed823f143cf3
MurtazaSyed <murtaza.qamarsyed@gmail.com>
2022-09-21 12:33:41 +0500

    Defining mongoDb schema for Users and Books

---
+++ models/Book.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema(
+  {
+    Name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+      },
+    pages: {
+        type: Number,
+        required: false,
+      },
+    price: {
+        type: Number,
+        required: true,
+      }
+    
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book", BookSchema);
